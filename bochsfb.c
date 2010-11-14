3e3bf57d9600b8326f5c9ac9de5bd654d3ba8ea9
jon <jon@a26a64fc-0d1c-11df-8ad2-4961b6296c36>
2010-11-14 18:12:22 +0000

    x
    git-svn-id: file:///srv/old-firewall-data/svn/tvbox/tvbox-v2.2-bochsfb@7 a26a64fc-0d1c-11df-8ad2-4961b6296c36

--- bochsfb.c
+++ bochsfb.c
@@ -35,25 +35,126 @@
 #include "bochsfb.h"
 #include "config.h"
 
+typedef struct VBVACONF32 {
+    uint32_t	u32Index,u32Value;
+} __attribute__((packed)) VBVACONF32;
+
+typedef struct _HGSMIBUFFERHEADER
+{
+    uint32_t    u32DataSize;            /* Size of data that follows the header. */
+
+    uint8_t     u8Flags;                /* The buffer description: HGSMI_BUFFER_HEADER_F_* */
+
+    uint8_t     u8Channel;              /* The channel the data must be routed to. */
+    uint16_t    u16ChannelInfo;         /* Opaque to the HGSMI, used by the channel. */
+
+    union {
+        uint8_t au8Union[8];            /* Opaque placeholder to make the union 8 bytes. */
+
+        struct
+        {                               /* HGSMI_BUFFER_HEADER_F_SEQ_SINGLE */
+            uint32_t u32Reserved1;      /* A reserved field, initialize to 0. */
+            uint32_t u32Reserved2;      /* A reserved field, initialize to 0. */
+        } Buffer;
+
+        struct
+        {                               /* HGSMI_BUFFER_HEADER_F_SEQ_START */
+            uint32_t u32SequenceNumber; /* The sequence number, the same for all buffers in the sequence. */
+            uint32_t u32SequenceSize;   /* The total size of the sequence. */
+        } SequenceStart;
+
+        struct
+        {                               /* HGSMI_BUFFER_HEADER_F_SEQ_CONTINUE and HGSMI_BUFFER_HEADER_F_SEQ_END */
+            uint32_t u32SequenceNumber; /* The sequence number, the same for all buffers in the sequence. */
+            uint32_t u32SequenceOffset; /* Data offset in the entire sequence. */
+        } SequenceContinue;
+    } u;
+
+} __attribute__((packed)) HGSMIBUFFERHEADER;
+
+typedef struct _HGSMIBUFFERTAIL
+{
+    uint32_t    u32Reserved;        /* Reserved, must be initialized to 0. */
+    uint32_t    u32Checksum;        /* Verifyer for the buffer header and offset and for first 4 bytes of the tail. */
+} __attribute__((packed)) HGSMIBUFFERTAIL;
+
+static uint32_t hgsmiHashBegin (void)
+{
+    return 0;
+}
+
+static uint32_t hgsmiHashProcess (uint32_t hash,
+                                  const void *pvData,
+                                  size_t cbData)
+{
+    const uint8_t *pu8Data = (const uint8_t *)pvData;
+
+    while (cbData--)
+    {
+        hash += *pu8Data++;
+        hash += (hash << 10);
+        hash ^= (hash >> 6);
+    }
+
+    return hash;
+}
+
+static uint32_t hgsmiHashEnd (uint32_t hash)
+{
+    hash += (hash << 3);
+    hash ^= (hash >> 11);
+    hash += (hash << 15);
+
+    return hash;
+}
+
+uint32_t HGSMIChecksum (uint32_t offBuffer,
+                        const HGSMIBUFFERHEADER *pHeader,
+                        const HGSMIBUFFERTAIL *pTail)
+{
+    uint32_t u32Checksum = hgsmiHashBegin ();
+
+    BUG_ON(sizeof(HGSMIBUFFERHEADER) != 16);
+    BUG_ON(sizeof(HGSMIBUFFERTAIL) != 8);
+
+    u32Checksum = hgsmiHashProcess (u32Checksum, &offBuffer, sizeof (offBuffer));
+    u32Checksum = hgsmiHashProcess (u32Checksum, pHeader, sizeof (HGSMIBUFFERHEADER));
+    u32Checksum = hgsmiHashProcess (u32Checksum, pTail, offsetof(HGSMIBUFFERTAIL, u32Checksum));
+
+    return hgsmiHashEnd (u32Checksum);
+}
+
+#define MAX_HEADS		64
+
 /* Module description/parameters */
 MODULE_AUTHOR("Jonathan Campbell <jonathan@impactstudiopro.com>");
 MODULE_DESCRIPTION("Framebuffer for Bochs/VirtualBox VBE extensions " BOCHSFB_BUILD_BANNER);
 MODULE_LICENSE("GPL");
 
 struct bochs_fb_info {
+	unsigned int		monitor;
+	unsigned long		base,size;
 	uint32_t		pseudo_palette[256];
+	unsigned char*		hgsmi_page;	/* WARNING: in VRAM */
 };
 
+static unsigned char		vbox_hgsmi = 0,vbox_video = 0;
 static struct pci_dev*		pci_vga_dev = NULL;
 static resource_size_t		apert_base;
 static unsigned int		apert_length;
 static unsigned int		apert_usable;
+static unsigned int		monitors = 1;
+static unsigned int		vbox_extensions = 0;
 static void*			aperture_stolen = NULL;
-struct fb_info*			bochs_fb = NULL;
+static void*			hgsmi_command = NULL;
+struct fb_info*			bochs_fb[MAX_HEADS];
 int				default_width=640,default_height=480;
 
 static int			ioport_index;
 static int			ioport_data;
+
+#define HGSMI_HOST			0x3B0
+#define HGSMI_GUEST			0x3D0
 
 #define VBE_DISPI_INDEX_ID		0x0
 #define VBE_DISPI_INDEX_XRES		0x1
@@ -75,6 +176,11 @@ static int			ioport_data;
 #define VBE_DISPI_LFB_ENABLED		0x40
 #define VBE_DISPI_NOCLEARMEM		0x80
 
+static unsigned int bochs_vbe_r(unsigned int index) {
+	outw(index,ioport_index);
+	return inw(ioport_data);
+}
+
 static void bochs_vbe_w(unsigned int index,unsigned int val) {
 	outw(index,ioport_index);
 	outw(val,ioport_data);
@@ -82,12 +188,15 @@ static void bochs_vbe_w(unsigned int index,unsigned int val) {
 
 static void bochs_unregister_fb(void) {
 	struct fb_info *f;
+	unsigned int head;
 
-	if ((f = bochs_fb) != NULL) {
-		unregister_framebuffer(f);	/* the code will not leave it in if it didn't succeed */
-		fb_dealloc_cmap(&f->cmap);
-		framebuffer_release(f);
-		bochs_fb = NULL;
+	for (head=0;head < monitors;head++) {
+		if ((f = bochs_fb[head]) != NULL) {
+			unregister_framebuffer(f);	/* the code will not leave it in if it didn't succeed */
+			fb_dealloc_cmap(&f->cmap);
+			framebuffer_release(f);
+			bochs_fb[head] = NULL;
+		}
 	}
 }
 
@@ -95,18 +204,23 @@ static void bochs_unmap_resources(void) {
 	if (aperture_stolen != NULL) {
 		iounmap(aperture_stolen);
 		aperture_stolen = NULL;
+#if 0
 		/* assumption: we wouldn't have ioremap()'d the aperture
 		 * if we were not able to claim it */
 		release_mem_region(apert_base,apert_usable);
+#endif
 	}
 }
 
 static int bochs_map_resources(void) {
+#if 0
 	if (!request_mem_region(apert_base,apert_usable,"bochs VBE aperture")) {
-		printk(KERN_ERR "bochsfb: cannot claim aperture\n");
+		printk(KERN_ERR "bochsfb: cannot claim aperture %llx-%llx\n",
+			apert_base,apert_base+apert_usable-1LL);
 		bochs_unmap_resources();
 		return 1;
 	}
+#endif
 
 	/* don't ask for the entire aperture, it might be too large for ioremap()
 	 * to handle and there's not that much stolen memory anyhow */
@@ -262,12 +376,13 @@ static struct fb_ops bochs_ops = {
 };
 
 static void fill_fb_default_mode(struct fb_info *fb,const int index) {
+	struct bochs_fb_info *par = fb->par;
 	struct fb_var_screeninfo *v = &fb->var;
 	struct fb_fix_screeninfo *f = &fb->fix;
 	int xres = default_width,yres = default_height;
 
 	/* caller already took care of the id field */
-	f->smem_start = apert_base;
+	f->smem_start = apert_base+par->base;
 	f->smem_len = apert_usable;
 	f->type = FB_TYPE_PACKED_PIXELS;
 	f->visual = FB_VISUAL_TRUECOLOR;
@@ -307,23 +422,29 @@ static void fill_fb_default_mode(struct fb_info *fb,const int index) {
 }
 
 /* register all framebuffers */
-static int bochs_register_fb(void) {
+static int bochs_register_fb(unsigned int monitor,unsigned long base,unsigned long size) {
 	struct bochs_fb_info *pf;
 	struct fb_info *f = NULL;
 	int i;
+
+	BUG_ON(monitor >= MAX_HEADS);
 
 	if ((f = framebuffer_alloc(sizeof(struct bochs_fb_info),&pci_vga_dev->dev)) == NULL) {
 		printk(KERN_ERR "Not enough memory to allocate a framebuffer\n");
 		goto err_out;
 	}
 	pf = f->par;
+	pf->monitor = monitor;
+	pf->base = base;
+	pf->size = size;
+	pf->hgsmi_page = NULL;
 	BUG_ON(pf == NULL);
 	f->pseudo_palette = pf->pseudo_palette;
 	/*		pci_set_drvdata(pci_vga_dev,f); */
 	f->flags = FBINFO_DEFAULT | FBINFO_HWACCEL_DISABLED;
 	f->fbops = &bochs_ops;
-	f->screen_base = aperture_stolen;
-	f->screen_size = apert_usable;
+	f->screen_base = aperture_stolen+base;/*aperture_stolen;*/
+	f->screen_size = size;/*apert_usable;*/
 	strcpy(f->fix.id,"Bochs VBE");
 	fill_fb_default_mode(f,i);
 
@@ -339,7 +460,7 @@ static int bochs_register_fb(void) {
 
 	bochs_check_var(&f->var,f);
 	bochs_set_par(f);
-	bochs_fb = f;
+	bochs_fb[monitor] = f;
 	f = NULL;
 
 	return 0;
@@ -398,6 +519,7 @@ static int looks_like_aperture(struct resource *res) {
 }
 
 static int __init bochs_identify_device(void) {
+	unsigned int id;
 	int i,idx = -1;
 
 	BUG_ON(pci_vga_dev == NULL);
@@ -416,12 +538,102 @@ static int __init bochs_identify_device(void) {
 	apert_base = pci_vga_dev->resource[idx].start;
 	apert_length = (pci_vga_dev->resource[idx].end - pci_vga_dev->resource[idx].start) + 1;
 
-	/* try to avoid situations where ioremap() fails because someone tols
+	/* try to avoid situations where ioremap() fails because someone told
 	 * VirtualBox to allocate 64MB of video memory */
 	apert_usable = apert_length;
 	if (apert_usable > (32*1024*1024))
 		apert_usable = (32*1024*1024);
 
+	/* if this is VirtualBox we may be able to make use of multi-monitor */
+	bochs_vbe_w(VBE_DISPI_INDEX_ID,0xBE00); /* VBOX_VIDEO */
+	bochs_vbe_w(VBE_DISPI_INDEX_ID,0xBE01); /* HGSMI */
+	id = bochs_vbe_r(VBE_DISPI_INDEX_ID);
+	printk(KERN_DEBUG "VBE ID 0x%04X\n",id);
+	vbox_extensions = ((id >= 0xB0C0 && id <= 0xB0C4) || (id >= 0xBE00 && id <= 0xBE02));
+	monitors = 1;
+
+	/* if this is VirtualBox, then ask how many monitors there are */
+	if (vbox_extensions) {
+		vbox_hgsmi = (id == 0xBE01);
+		vbox_video = (id == 0xBE00);
+
+		id = bochs_vbe_r(VBE_DISPI_INDEX_VBOX_VIDEO);
+		if (id == 0) id = 1;
+	}
+
+	return 0;
+}
+
+static unsigned int hgsmi_allocd = 0;
+static HGSMIBUFFERHEADER *hgsmi_cmd_begin(size_t cblen,void **data) {
+	HGSMIBUFFERHEADER *h = (HGSMIBUFFERHEADER*)hgsmi_command;
+	if ((cblen+sizeof(HGSMIBUFFERHEADER)+sizeof(HGSMIBUFFERTAIL)) >= 4096)
+		return NULL;
+
+	hgsmi_allocd = (unsigned int)cblen;
+	memset(h,0,sizeof(HGSMIBUFFERHEADER));
+	*data = (void*)((char*)hgsmi_command+sizeof(HGSMIBUFFERHEADER));
+	h->u32DataSize = 8;
+	return h;
+}
+
+static void hgsmi_cmd_submit(void) {
+	/* complete tail checksum and submit to H/W */
+	HGSMIBUFFERTAIL *t = (HGSMIBUFFERTAIL*)((char*)hgsmi_command+sizeof(HGSMIBUFFERHEADER)+hgsmi_allocd);
+	t->u32Reserved = 0;
+	t->u32Checksum = HGSMIChecksum(((uint32_t)((char*)hgsmi_command - (char*)aperture_stolen)),(HGSMIBUFFERHEADER*)hgsmi_command,t);
+	outl(((uint32_t)((char*)hgsmi_command - (char*)aperture_stolen)),HGSMI_GUEST);
+	inl(HGSMI_GUEST);
+	inl(HGSMI_GUEST);
+	inl(HGSMI_GUEST);
+	inl(HGSMI_GUEST);
+}
+
+static uint32_t hgsmi_query_conf32(uint32_t index) {
+	HGSMIBUFFERHEADER *h;
+	void *dp = NULL;
+
+	h = hgsmi_cmd_begin(8,&dp);
+	if (h) {
+		volatile VBVACONF32 *v = (VBVACONF32*)dp;
+		BUG_ON(dp == NULL);
+		BUG_ON(sizeof(HGSMIBUFFERHEADER) != 16);
+		BUG_ON(sizeof(HGSMIBUFFERTAIL) != 8);
+		v->u32Index = 0;		/* VBOX_VBVA_CONF32_MONITOR_COUNT */
+		v->u32Value = 0;
+		h->u8Flags = 0x00;		/* single buffer */
+		h->u8Channel = 0x02;		/* 0x02 = VBVA */
+		h->u16ChannelInfo = 0x01;	/* VBVA_QUERY_CONF32 */
+		barrier();
+		hgsmi_cmd_submit();
+		barrier();
+		return v->u32Value;
+	}
+	else {
+		printk(KERN_ERR "Failed to alloc HGSMI space for monitor query\n");
+		return (uint32_t)0xFFFFFFFFUL;
+	}
+}
+
+/* NTS: assume caller already checked vbox_hgsmi */
+/* NTS: VirtualBox code suggests that the command buffer must reside in
+ *      VGA video RAM even though we give the host a raw physical memory
+ *      address */
+static int check_hgsmi(void) { 
+	uint32_t v32;
+
+	BUG_ON(!vbox_hgsmi);
+
+	if (apert_usable < (1*1024*1024))
+		return 1;
+
+	apert_usable -= 4096;
+	hgsmi_command = (void*)((char*)aperture_stolen + apert_usable);
+	printk(KERN_DEBUG "Allocating 4KB for HGSMI command buffer\n");
+
+	/* how many monitors? */
+	v32 = hgsmi_query_conf32(0);		/* VBOX_VBVA_CONF32_MONITOR_COUNT */
+	printk(KERN_INFO "%u monitors\n",v32);
 	return 0;
 }
 
@@ -446,15 +658,9 @@ static int __init bochs_detect_ioports(void) {
 	return 1;
 }
 
-#if 0
-static unsigned int bochs_vbe_r(unsigned int index) {
-	outw(ioport_index,index);
-	return inw(ioport_data);
-}
-#endif
-
 static int __init bochs_init(void)
 {
+	memset(bochs_fb,0,sizeof(bochs_fb));
 	if ((pci_vga_dev = bochs_find_device()) == NULL)
 		return -ENODEV;
 
@@ -480,13 +686,7 @@ static int __init bochs_init(void)
 		return -ENODEV;
 	}
 
-	/* register and alloc a framebuffer for each */
-	if (bochs_register_fb()) {
-		printk(KERN_ERR "Problem registering framebuffers\n");
-		bochs_free_dev();
-		return -ENOMEM;
-	}
-
+#if 0
 #ifdef CONFIG_MTRR
 	/* help performance by covering the aperture with
 	 * a write-combining MTRR. it might already be there,
@@ -494,6 +694,32 @@ static int __init bochs_init(void)
 	 * is slightly slower, who cares? */
 	mtrr_add(apert_base,apert_usable,MTRR_TYPE_WRCOMB,1);
 #endif
+#endif
+
+	/* VirtualBox has extensions of it's own that enable multiple monitors
+	 * and a memory-based command/response system */
+	if (vbox_extensions) {
+		if (vbox_hgsmi) {
+			if (!check_hgsmi()) {
+				printk(KERN_WARNING "bochsfb: HGSMI functionality disabled\n");
+				vbox_hgsmi = 0;
+			}
+		}
+	}
+
+	/* register and alloc a framebuffer for each */
+	if (monitors == 1) {
+		printk(KERN_ERR "bochsfb: setting up single-monitor setup\n");
+		if (bochs_register_fb(0,0,apert_usable)) {
+			printk(KERN_ERR "Problem registering framebuffers\n");
+			bochs_free_dev();
+			return -ENOMEM;
+		}
+	}
+	else {
+		/* FIXME */
+		monitors = 0;
+	}
 
 	return 0;
 }
